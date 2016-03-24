1d9cebec6c3f28818fbf7a56b34e4883c482ffa1
Nicole Chiang <nic.chiang@gmail.com>
2016-03-24 11:08:26 -0400

    attempting custom devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_for :users, :controllers => { registrations: "registrations" }
 
   root "users#index"   ## this will probably change. maybe a splash?
 
