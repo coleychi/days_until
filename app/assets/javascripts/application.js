bf00b0c9051195d685e2cae2b4c8a8ba3242394f
Nicole Chiang <nic.chiang@gmail.com>
2016-03-24 16:27:06 -0400

    removing bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
